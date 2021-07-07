413819c01ddf0c7166a9f083474eb51153f6c99a
Tzikas <niko.tzikas@gmail.com>
2021-07-07 12:13:30 -0400

    context and history

--- 7-7/src/Treat.js
+++ 7-7/src/Treat.js
@@ -1,17 +1,23 @@
 import React, { useContext } from 'react';
 import TheContext from './TheContext'
+import { useHistory } from "react-router-dom";
 
 
 
 function Treat(props) {
 
     const { setHijoColor } = useContext(TheContext)
-    console.log(useContext(TheContext))
+
+    let history = useHistory();
+    console.log(history)
+
 
     return (
         <div style={{ backgroundColor: props.color }}>
             Treat 🦴
             <button onClick={() => setHijoColor(("#" + ((1 << 24) * Math.random() | 0).toString(16)))} >Change Hijos color</button>
+
+            <button onClick={() => history.push('/this works!')} >Home</button>
         </div>
     );
 }
